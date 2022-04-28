6c027dfeb915f6a97d1a17360a915b93b9f02459
guohailong <1105744725@qq.com>
2022-04-28 15:27:32 +0800

    commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# 吾爱破解自动签到Github Action 版
+
+## 今日签到状态
+![52Pojie Auto Sign](https://github.com/RoadIsLong/52pojie_Sign/workflows/52Pojie%20Auto%20Sign/badge.svg)
+
+## 使用说明
+1.FORK该仓库，然后点击你的仓库右上角的 Settings，找到 Secrets 这一项，点击action添加一个密钥。其中 POJIE 存放你的 COOKIE
+ ![image](https://user-images.githubusercontent.com/30728105/165697595-302d3965-c456-4ec0-8733-533256b6041b.png)
+
+2.设置好环境变量后点击你的仓库上方的 Actions 选项，第一次打开需要点击 I understand... 按钮，确认在 Fork 的仓库上启用 GitHub Actions 。
+
+3.然后任意发起一次提交，可以选择修改readme文件
+
+4.至此自动签到就搭建完毕了，可以再次点击Actions查看工作记录，如果有52Pojie Auto Sign则说明workflow创建成功了。点击右侧记录可以查看详细签到情况
+ ![image](https://user-images.githubusercontent.com/30728105/165697734-a38d6d46-db0e-40a1-8b21-e0d50443de36.png)
---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import os
+import requests
+from bs4 import BeautifulSoup
+
+def sign(cookie):
+    result = ""
+    headers = {
+        "Cookie": cookie,
+        "ContentType": "text/html;charset=gbk",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+    }
+    requests.session().put(
+        "https://www.52pojie.cn/home.php?mod=task&do=apply&id=2", headers=headers
+    )
+    fa = requests.session().put(
+        "https://www.52pojie.cn/home.php?mod=task&do=draw&id=2", headers=headers
+    )
+    fb = BeautifulSoup(fa.text, "html.parser")
+    fc = fb.find("div", id="messagetext").find("p").text
+    if "您需要先登录才能继续本操作" in fc:
+        result += "Cookie 失效"
+    elif "恭喜" in fc:
+        result += "签到成功"
+    elif "不是进行中的任务" in fc:
+        result += "不是进行中的任务"
+    else:
+        result += "签到失败"
+    return result
+
+def main():
+    b = os.environ['POJIE']
+    cookie = b
+    sign_msg = sign(cookie=cookie)
+    print(sign_msg)
+
+
+if __name__ == "__main__":
+    print('start')
+    main()
+    print('end')
